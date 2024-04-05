e010c395cd97461b427a5d7d45a4940baf2fa3ac
abhinandansanoor <155422032+abhinandansanoor@users.noreply.github.com>
2024-04-05 09:31:24 +0530

    Update 1_click.sh

--- 1_click.sh
+++ 1_click.sh
@@ -20,7 +20,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 pushd "${SCRIPT_DIR}" 1> /dev/null
 
-chmod +x "${BASH_SOURCE[0]}"
+# Check if script has execute permission and add it if necessary
+if [ ! -x "$0" ]; then
+  echo "Adding execute permission to 1_click.sh..."
+  chmod +x "$0"
+fi
 
 echo -n "Please wait..."
 git submodule update --init --recursive &> /dev/null || true
